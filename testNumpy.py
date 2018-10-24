55f86f4f8ac2998fa47e1e19276d8a5436c7be03
rexfa <rex_fa@163.com>
2018-10-24 17:07:48 +0800

    tf test

---
+++ testNumpy.py
@@ -0,0 +1,6 @@
+import numpy as npm
+x = [1,1,2,4,3.141592654,4000000]
+y = [1,4,8,400,2,1]
+t = npm.arctan2(x,y)
+print(t)
+
--- testTensorflow.py
+++ testTensorflow.py
@@ -1,6 +1,7 @@
+
 import tensorflow  as tfm
 import numpy as npm
-import pylab as pl
+import matplotlib.pyplot as plt
 #create data
 x_data=npm.random.rand(100).astype(npm.float32)
 y_data=x_data*0.1+0.3
@@ -22,16 +23,23 @@ session.run(init)
 
 #train
 #dispaly
-pl.figure(1)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
 t = npm.arctan2(x_data, y_data)
-pl.scatter(x_data,y_data)
+
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
 for step in range(201):
     session.run(train)
     if step%20==0:
-        xp = pl.linspace(0, 1, 3)
+        xp = npm.linspace(0, 1, 3)
         yp = xp*session.run(Weights)+session.run(biases)
-        pl.plot(xp,yp)
+        ax.plot(xp,yp)
+        #plt.ion()
+        #plt.show()
         print(step,session.run(Weights),session.run(biases))
+        plt.pause(1)
 
 #show
-pl.show()
+#plt.show()
